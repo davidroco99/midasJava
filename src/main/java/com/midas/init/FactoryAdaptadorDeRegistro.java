563aca714917381db196bdfe8adbfbb3caf1c7a1
davidroco99 <davidroco99@gmail.com>
2020-11-05 18:45:44 -0300

    implemetación del adaptador

--- src/main/java/com/midas/init/FactoryAdaptadorDeRegistro.java
+++ src/main/java/com/midas/init/FactoryAdaptadorDeRegistro.java
@@ -1,7 +1,38 @@
-
-
 package com.midas.init;
 
-public interface FactoryAdaptadorDeRegistro {
-           
+/**
+ *
+ * @author david roco
+ */
+public class FactoryAdaptadorDeRegistro {
+
+    private static FactoryAdaptadorDeRegistro instancia;
+
+    //singleton
+    public static FactoryAdaptadorDeRegistro getInstance() {
+        if (instancia == null) {
+            instancia = new FactoryAdaptadorDeRegistro();
+        }
+        return instancia;
+    }
+
+    public Object optenerAdaptadorDeRegistro(String t) {
+        switch (t) {
+            case "file":
+                AdaptadorRegistroFileTxt newRegistrarMensaje = new AdaptadorRegistroFileTxt();
+                return newRegistrarMensaje;
+
+            case "console":
+               AdaptadorRegistroConsola newRegistrarMensajeConsole = new AdaptadorRegistroConsola();
+                return newRegistrarMensajeConsole;
+
+            case "db":
+                AdaptadorRegistroBaseDatos  newRegistrarMensajeDB = new AdaptadorRegistroBaseDatos();
+                return newRegistrarMensajeDB;
+
+            default:
+                return null;
+
+        }
+    }
 }
